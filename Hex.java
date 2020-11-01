303b134c201b8170d318c689d64a8ee1f672f509
Cindy Zheng <czheng10@stuy.edu>
2020-10-31 21:21:02 -0400

    inputs

---
+++ Hex.java
@@ -0,0 +1,19 @@
+/**************************************************************************    
+Cindy Zheng                                                                    
+Cybersecurity                                                                  
+XOR Strings                                                                   
+                                                                               
+Given three strings mode, text, and key: it will produce an output string by x\
+r'ing corresponding values together.                                           
+ *************************************************************************/
+
+public class Hex{
+
+    public static void main(String[]args){
+        String mode = args[0];
+        String text = args[1];
+        String key = args[2];
+    }
+
+
+}
